663559eb9a0b03237e36ba9332c226c1ea98cab8
Roman Omelchuk <roman.omelchuk91@gmail.com>
2023-01-04 13:57:46 +0100

    §

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React  from 'react';
+import Counter from './Counter/Counter';
+
+// export class App extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
+// }
+
+const App = () => {
+  <>
+    <h1>Please leave feedback</h1>
+    <Counter />
+  </>;
 };
+
+export default App;
